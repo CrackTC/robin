11345f213ebfe1a96dca83944cd525b27aaf34e3
CrackTC <chenrke369@gmail.com>
2024-01-14 03:23:35 +0800

    refactor: IIFE

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { load_handlers } from "./handlers/index.ts";
 import { setup_http } from "./http.ts";
 import { setup_ws_api, setup_ws_event } from "./ws.ts";
 
-load_handlers().then(load_api).then(() => {
+(async () => {
+  await load_handlers();
+  await load_api();
   watch_config();
   if (get_config().ws_addr) {
     setup_ws_event();
     setup_ws_api();
   }
   setup_http();
-});
+})();
