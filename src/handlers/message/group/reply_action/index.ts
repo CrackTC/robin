be02e2b2251565d2e79f72398fa268e6481aa9d9
CrackTC <chenrke369@gmail.com>
2024-02-15 09:27:38 +0800

    fix(reply_action): don't use at

--- src/handlers/message/group/reply_action/index.ts
+++ src/handlers/message/group/reply_action/index.ts
@@ -1,6 +1,5 @@
 import { get_msg } from "../../../../onebot/cqhttp.ts";
 import {
-  mk_at,
   mk_text,
   send_group_message,
 } from "../../../../onebot/cqhttp.ts";
@@ -8,6 +7,7 @@ import { GroupMessageEvent } from "../../../../onebot/types/event/message.ts";
 import { Message } from "../../../../onebot/types/message.ts";
 import { GroupEventHandler } from "../types.ts";
 import { error } from "../../../../utils.ts";
+import { get_group_member_info } from "../../../../onebot/cqhttp.ts";
 
 const reply_regex = /\[CQ:reply,id=(\d+)\]/;
 
@@ -21,7 +21,7 @@ const get_reply_id = (message: Message) => {
 
 const get_text = (message: Message) =>
   typeof message === "string"
-    ? message.replace(reply_regex, "").trim()
+    ? message.replace(/\[CQ:[^\]]*\]/, "").trim()
     : message.map((seg) => seg.type === "text" ? seg.data.text.trim() : "")
       .join("");
 
@@ -35,20 +35,29 @@ const handle_func = async (event: GroupMessageEvent) => {
   const reply_id = get_reply_id(event.message);
   if (!reply_id) return;
 
+  const source_name = event.sender.card ?? event.sender.nickname;
+
   const msg = await get_msg(parseInt(reply_id));
   if (!msg) {
     error("failed to get message");
     return;
   }
-  const target_id = msg.sender.user_id;
+  const info = await get_group_member_info(event.group_id, msg.sender.user_id);
+  if (!info) {
+    error("failed to get member info");
+    return;
+  }
+
+  const target_name = info.card ?? info.nickname;
 
   const message: Message = [
-    mk_at(event.sender.user_id),
-    mk_text(" " + parts[0]),
-    mk_at(target_id),
+    mk_text(
+      `${source_name} ${parts[0]} ${target_name}${
+        parts[1] ? " " + parts[1] : ""
+      }`,
+    ),
   ];
 
-  if (parts.length > 1) message.push(mk_text(" " + parts[1]));
   send_group_message(event.group_id, message);
 };
 
