99351855944b2189f127d1ac600a3f3cf3422183
CrackTC <chenrke369@gmail.com>
2024-03-05 08:33:31 +0800

    chore!: move lib to separate repo

--- src/api/lib/auth/index.ts
+++
@@ -1,5 +0,0 @@
-import { verify_token, wrap } from "/wrappers.ts";
-import { ApiHandler } from "/api/types.ts";
-import { ok } from "/api/common.ts";
-
-export default () => wrap<ApiHandler>(() => ok()).with(verify_token);
